40630acfd27166f544da797612d93d79216852af
kimcon <conb1909886@student.ctu.edu.vn>
2022-09-19 17:22:39 +0700

    add for.c

---
+++ for.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+          int i;
+          int n;
+           printf ("nhap vao n ", &n );
+          scanf ("%d" ,&n);
+          for (i=0;i<=n;i++){
+}  printf ("%d" ,i);
+   return 0;
+}
+ 
